e20f14dc9eec843fb320b23bd9eec4f30d7457ff
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-11-17 18:38:50 -0700

    adds theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ThemeProvider } from "./components/theme-provider";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
+    <ThemeProvider
+      defaultTheme="dark"
+      storageKey="vite-ui-theme"
+    >
+      <div className="border border-foreground">
+        <h1>Hello World!</h1>
       </div>
-      <h1>Solomon Schuster</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>This is a work in progress</p>
-      </div>
-      <p className="read-the-docs">Check back later for an actual website</p>
-    </>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+// #9cc6e6
+// #0d0101
+// #cecef5
