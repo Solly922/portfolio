8449d402f2cc605415a1c88aecad9243d81eca1c
Solomon <solomon.schuster01@gmail.com>
2024-06-09 21:29:29 -0700

    starts project cards

---
+++ src/components/hero.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Resume from "@/assets/Solomon-Schuster-Resume.pdf";
+
+function Hero() {
+  return (
+    <div className="flex justify-center items-center grow flex-col gap-10 animate-fade-in">
+      <div className="text-center space-y-2 tracking-wider">
+        <h1 className="text-6xl font-extralight filter drop-shadow-glow">
+          Solomon Schuster
+        </h1>
+        <h3 className="text-xl">Software Engineer</h3>
+      </div>
+
+      <div>
+        <a href={Resume}>
+          <Button variant="outline">Resume</Button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/scenes/main/components/projects.tsx
@@ -0,0 +1,16 @@
+import FancyCard from "@/components/fancy-card";
+
+function Projects() {
+  return (
+    <div className="flex flex-col w-full">
+      <FancyCard
+        header="Ingot"
+        description="Lunasonde (source code unavailable)"
+      >
+        This is a test
+      </FancyCard>
+    </div>
+  );
+}
+
+export default Projects;
