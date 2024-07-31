0c539af8c362bcd0e9fe39a965536a9ee38b3292
Solly922 <solomon.schuster01@gmail.com>
2024-07-30 22:00:50 -0700

    adds sparkles to card

--- src/components/fancy-card.tsx
+++ src/components/fancy-card.tsx
@@ -14,6 +14,27 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { clsx } from "clsx";
+import { SparklesCore } from "@/components/ui/sparkles";
+import { memo } from "react";
+
+const dividerStyles = {
+  primaryLeft:
+    "[mask-image:radial-gradient(140px_at_left,white,transparent)] left-0 bg-gradient-to-r",
+  primaryRight:
+    "[mask-image:radial-gradient(140px_at_right,white,transparent)] right-0 bg-gradient-to-l",
+  secondaryLeft:
+    "[mask-image:radial-gradient(65px_at_left,white,transparent)] left-0 bg-gradient-to-r ",
+  secondaryRight:
+    "[mask-image:radial-gradient(65px_at_right,white,transparent)] right-0 bg-gradient-to-l",
+  sparklesParentLeft:
+    "-right-10 [mask-image:radial-gradient(100px_at_right,white,transparent)]",
+  sparklesParentRight:
+    "-left-10 [mask-image:radial-gradient(100px_at_left,white,transparent)]",
+  sparklesLeft:
+    "-right-10 [mask-image:radial-gradient(200px_at_left,white,transparent)]",
+  sparklesRight:
+    "-left-10 [mask-image:radial-gradient(200px_at_right,white,transparent)]",
+};
 
 export interface FancyCardProps {
   children?: React.ReactNode;
@@ -34,9 +55,10 @@ function FancyCard({
 }: FancyCardProps) {
   return (
     <div className="w-full relative">
+      <Sparkles />
       <Card
         style={{ float: float || "right" }}
-        className={clsx("max-w-[1000px]")}
+        className={clsx("max-w-[1000px]", float === "left" ? "ml-10" : "mr-10")}
       >
         <CardHeader>
           <CardTitle className="uppercase">{header}</CardTitle>
@@ -69,7 +91,7 @@ function FancyCard({
                 </CarouselContent>
 
                 <CarouselPrevious className="left-0" />
-                <CarouselNext style={{ right: "0px !important" }} />
+                <CarouselNext style={{ right: "0px" }} />
               </Carousel>
             </div>
           )}
@@ -82,3 +104,50 @@ function FancyCard({
 }
 
 export default FancyCard;
+
+const MemoizedSparklesCore = memo(SparklesCore);
+
+function Sparkles({ float }: { float?: "left" | "right" }) {
+  return (
+    <div
+      className={clsx(
+        "absolute -z-30 top-[50%] -translate-y-[50%] h-full bg-gradient-to-b from-transparent from [5%] to-[95%] via-indigo-500 to-transparent",
+        float === "left" ? "left-0" : "right-0"
+      )}
+    >
+      <div
+        className={clsx(
+          "w-28 h-full absolute top-1/2 -translate-y-1/2 from-indigo-400 via-transparent to-transparent -z-10 opacity-50",
+          float === "left"
+            ? dividerStyles.primaryLeft
+            : dividerStyles.primaryRight
+        )}
+      />
+      <div
+        className={clsx(
+          "w-10 h-1/2 absolute top-1/2 -translate-y-1/2 from-cyan-400 via-transparent to-transparent -z-20 opacity-100",
+          float === "left"
+            ? dividerStyles.secondaryLeft
+            : dividerStyles.secondaryRight
+        )}
+      />
+      <div
+        className={clsx(
+          "w-10 h-full top-1/2 -translate-y-1/2 absolute",
+          float === "left"
+            ? dividerStyles.sparklesParentLeft
+            : dividerStyles.sparklesParentRight
+        )}
+      >
+        <MemoizedSparklesCore
+          background="transparent"
+          minSize={0.1}
+          maxSize={1}
+          particleDensity={1200}
+          // particleColor="#d34"
+          className={clsx("h-full")}
+        />
+      </div>
+    </div>
+  );
+}
